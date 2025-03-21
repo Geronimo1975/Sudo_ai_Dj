85a1477339b0f8aae5fdee1408276e9e7cb20ca3
George-Sebastian <155230741+Geronimo1975@users.noreply.github.com>
2025-03-21 11:22:18 +0100

    Update dev.py

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -23,7 +23,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PWD'),
+        'PASSWORD': env.str('DB_PASSWORD'),
         'HOST': env.str('DB_HOST'),
         'PORT': env.str('DB_PORT'),
     }
